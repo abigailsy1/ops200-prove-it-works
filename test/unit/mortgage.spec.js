d126e1b9a1d6a781dcad203247534abede2ef174
Abby <abigailsy1@yahoo.com>
2018-06-18 22:01:17 -0700

    first commit

---
+++ test/unit/mortgage.spec.js
@@ -0,0 +1,25 @@
+const expect = require('chai').expect;
+const Mortgage = require('../../src/js/lib/Mortgage');
+
+describe('Mortgage Calculator', () => {
+    let mortgage = null;
+
+    beforeEach(() => {
+        mortgage = new Mortgage(500000, 5.5, 50);
+    });
+    it('should have a mortgage function', () => {
+        expect(mortgage.monthlyPayment).to.exist;
+    });
+
+    it('should have a principal = 5500,000', () => {
+        expect(mortgage.principal).to.equal(500000)
+    });
+
+    it('should have the interest = 5.5%', () => {
+        expect(mortgage.interest).equal(5.5);
+    });
+
+    it('should have the term = 50 years', () => {
+        expect(mortgage.term).to.equal(50);
+    });
+});
